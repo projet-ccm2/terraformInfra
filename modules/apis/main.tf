117af0bc728a8b38652b0d2326439e130ebeba43
Matthieu Marin <matthieumarin51@gmail.com>
2025-09-19 10:19:32 +0200

    cd

--- modules/apis/main.tf
+++ modules/apis/main.tf
@@ -21,7 +21,6 @@ locals {
 resource "google_project_service" "required" {
   for_each           = toset(local.required_services)
   project            = var.project_id
-  service            = each.value
   disable_on_destroy = false
 }
 
