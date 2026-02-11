e9a309d4458375cce262570c342e404987acc489
Matthieu Marin <matthieumarin51@gmail.com>
2026-02-11 11:54:53 +0100

    fix

--- modules/apis/main.tf
+++ modules/apis/main.tf
@@ -23,15 +23,14 @@ resource "google_project_service" "required" {
   project            = var.project_id
   service            = each.value
   disable_on_destroy = false
-  
-  depends_on = [google_project_service.required]
 }
 
 # Enable other services (they depend on Cloud Resource Manager)
 resource "google_project_service" "this" {
+  for_each           = toset(local.services)
   project            = var.project_id
   service            = each.value
   disable_on_destroy = false
-  
+
   depends_on = [google_project_service.required]
 }
